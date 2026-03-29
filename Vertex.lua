bb1b275849277c1a5af4118c50efa0b9212bbec0
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-03-28 23:50:04 -0300

    Update Vertex.lua

--- Vertex.lua
+++ Vertex.lua
@@ -24,7 +24,7 @@ local MainStroke = Instance.new("UIStroke", Main)
 MainStroke.Color = Color3.fromRGB(50,50,50)
 MainStroke.Thickness = 2
 
---// sidebar
+--// sidebar (ÚNICO panel ahora)
 local Sidebar = Instance.new("Frame")
 Sidebar.Parent = Main
 Sidebar.Size = UDim2.new(0.25, 0, 1, 0)
@@ -32,17 +32,3 @@ Sidebar.BackgroundColor3 = Color3.fromRGB(20,20,20)
 
 local SideCorner = Instance.new("UICorner", Sidebar)
 SideCorner.CornerRadius = UDim.new(0, 12)
-
---// content
-local Content = Instance.new("Frame")
-Content.Parent = Main
-Content.Size = UDim2.new(0.75, 0, 1, 0)
-Content.Position = UDim2.new(0.25, 0, 0, 0)
-Content.BackgroundColor3 = Color3.fromRGB(18,18,18)
-
-local ContentCorner = Instance.new("UICorner", Content)
-ContentCorner.CornerRadius = UDim.new(0, 12)
-
-local ContentStroke = Instance.new("UIStroke", Content)
-ContentStroke.Color = Color3.fromRGB(40,40,40)
-ContentStroke.Thickness = 1.5
