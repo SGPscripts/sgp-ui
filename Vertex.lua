6b14b524ac621004c417264c2ca3480f686bfbae
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-03-28 23:51:31 -0300

    Update Vertex.lua

--- Vertex.lua
+++ Vertex.lua
@@ -10,11 +10,11 @@ Blur.Parent = Lighting
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Parent = game.CoreGui
 
---// main
+--// main (MUCHO MÁS GRANDE)
 local Main = Instance.new("Frame")
 Main.Parent = ScreenGui
-Main.Size = UDim2.new(0.85, 0, 0.65, 0)
-Main.Position = UDim2.new(0.075, 0, 0.175, 0)
+Main.Size = UDim2.new(0.95, 0, 0.85, 0) -- más grande
+Main.Position = UDim2.new(0.025, 0, 0.075, 0) -- centrado
 Main.BackgroundColor3 = Color3.fromRGB(15,15,15)
 
 local MainCorner = Instance.new("UICorner", Main)
@@ -24,10 +24,10 @@ local MainStroke = Instance.new("UIStroke", Main)
 MainStroke.Color = Color3.fromRGB(50,50,50)
 MainStroke.Thickness = 2
 
---// sidebar (ÚNICO panel ahora)
+--// sidebar
 local Sidebar = Instance.new("Frame")
 Sidebar.Parent = Main
-Sidebar.Size = UDim2.new(0.25, 0, 1, 0)
+Sidebar.Size = UDim2.new(0.2, 0, 1, 0) -- un poco más fina
 Sidebar.BackgroundColor3 = Color3.fromRGB(20,20,20)
 
 local SideCorner = Instance.new("UICorner", Sidebar)
